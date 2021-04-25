0404795b6e8e89be867c2139fec662417c70757c
Wh1isper <9573586@qq.com>
2021-04-26 00:56:23 +0800

    优化：节省缓存资源

--- sparksampling/core/dataio/csv_dataio.py
+++ sparksampling/core/dataio/csv_dataio.py
@@ -6,13 +6,16 @@ class CsvDataIO(BaseDataIO):
     def __init__(self, spark, path, *args, **kwargs):
         super(CsvDataIO, self).__init__(spark, path, args, kwargs)
         self.header = kwargs.get('with_header', True)
+        self.__df: DataFrame = None
 
     def _read(self, *args, **kwargs):
         df = self.spark.read.csv(self.path, header=self.header).cache()
-        df.show(5)
+        self.__df = df
         return df
 
     def _write(self, df: DataFrame):
-        df.show(5)
+        if self.__df:
+            self.__df.unpersist()
+        df.repartition(self.__df.rdd.getNumPartitions())
         df.write.csv(self.write_path, mode='overwrite', header=self.header)
         return self.write_path
