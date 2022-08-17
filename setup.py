1cdc580f3b6b59351232ce7dde6298a5fd2d020d
Wh1isper <9573586@qq.com>
2022-08-17 17:13:03 +0800

    release 0.1.7.3

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/Wh1isper/pyspark-sampling',
-    version='0.1.7.2',
+    version='0.1.7.3',
     zip_safe=False,
 )
