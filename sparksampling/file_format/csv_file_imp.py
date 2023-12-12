e351354ea9dd734901fa19e9ba79f8166f849e94
Wh1isper <9573586@qq.com>
2023-12-12 21:56:46 +0800

    Support csv multi line by default

--- sparksampling/file_format/csv_file_imp.py
+++ sparksampling/file_format/csv_file_imp.py
@@ -5,16 +5,19 @@ from sparksampling.file_format.output_adapter import OutputAdapterMixin
 
 
 class CsvFileImpSpark(SparkBaseFileFormat, OutputAdapterMixin):
-    cls_args = ["with_header", "sep"]
+    cls_args = ["with_header", "sep", "multi_line"]
 
     def __init__(self, spark, *args, **kwargs):
         super(CsvFileImpSpark, self).__init__(spark, *args, **kwargs)
         self.with_header = kwargs.pop("with_header", False)
         default_sep = "\001" if not os.getenv("COMMA_SEP") else ","
         self.sep = kwargs.pop("sep", default_sep)
+        self.multi_line = os.getenv("ENABLE_MULTI_LINE", "True") in ["True", "true"]
 
     def read(self, input_path):
-        return self.spark.read.csv(input_path, sep=self.sep, header=self.with_header)
+        return self.spark.read.csv(
+            input_path, sep=self.sep, header=self.with_header, multiLine=self.multi_line
+        )
 
     def write(self, df, output_path, output_col=None):
         if output_col:
